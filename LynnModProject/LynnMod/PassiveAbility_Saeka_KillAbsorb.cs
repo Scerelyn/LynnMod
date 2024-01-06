8b84cf0bf126893f7b4912ff5a4e19b794d744aa
Scerelyn <scerelyn@gmail.com>
2024-01-05 23:03:40 -0600

    add inital passives for saeka

---
+++ LynnModProject/LynnMod/PassiveAbility_Saeka_KillAbsorb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruina
+{
+    public class PassiveAbility_Saeka_KillAbsorb : PassiveAbilityBase
+    {
+        public static string Name = "Absorbtion";
+        public static string Desc = "On kill, increase the maximum value of passive 'Built Strength' by 1";
+
+        public override void OnKill(BattleUnitModel target)
+        {
+            var passive = owner.passiveDetail.PassiveList.First(p => p is PassiveAbility_Saeka_PowerMaintain) as PassiveAbility_Saeka_PowerMaintain;
+            passive.buffMax += 1;
+        }
+    }
+}
---
+++ LynnModProject/LynnMod/PassiveAbility_Saeka_PowerMaintain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruina
+{
+    public class PassiveAbility_Saeka_PowerMaintain : PassiveAbilityBase
+    {
+        public static string Name = "Built Strength";
+        public static string Desc = "On turn end, keep stacks of Strength, Endurance, and Haste on the next turn (max 5).";
+        public int buffMax = 5;
+        public override void OnRoundEnd()
+        {
+            Dictionary<KeywordBuf, BattleUnitBuf> buffs = owner.bufListDetail.GetActivatedBufList()
+                .Where(b => b.bufType == KeywordBuf.Strength || b.bufType == KeywordBuf.Endurance || b.bufType == KeywordBuf.Quickness)
+                .ToDictionary(b => b.bufType, b => b);
+
+            foreach(KeywordBuf key in buffs.Keys)
+            {
+                switch (key)
+                {
+                    case KeywordBuf.Strength:
+                    case KeywordBuf.Endurance:
+                    case KeywordBuf.Quickness:
+                        owner.bufListDetail.AddKeywordBufByEtc(key, Math.Min(buffs[key].stack, buffMax));
+                        break;
+                }
+            }
+        }
+    }
+}
