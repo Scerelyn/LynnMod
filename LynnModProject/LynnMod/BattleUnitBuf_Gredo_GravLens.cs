41a8f810501f46e8f4edb0c8740cfa5dd5cf00ea
Scerelyn <scerelyn@gmail.com>
2024-03-15 20:53:13 -0500

    add localizations, fix missing icon info, fix formatting

--- LynnModProject/LynnMod/BattleUnitBuf_Gredo_GravLens.cs
+++ LynnModProject/LynnMod/BattleUnitBuf_Gredo_GravLens.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace Ruina
     {
         public static string Name = "Umbral Lens";
         public static string Desc = "If this character takes no action, receive {0} damage";
+
+        protected override string keywordId => "Gredo_Umbral";
+        protected override string keywordIconId => "Umbral";
+        public override BufPositiveType positiveType => BufPositiveType.Negative;
 
         private bool performedAction = false;
 
@@ -27,6 +32,14 @@ namespace Ruina
         {
             performedAction = true;
             base.OnRollDice(behavior);
+        }
+
+        public override void Init(BattleUnitModel owner)
+        {
+            base.Init(owner);
+            typeof(BattleUnitBuf).GetField("_bufIcon", AccessTools.all).SetValue(this, Initializer.ArtWorks["Umbral"]);
+            typeof(BattleUnitBuf).GetField("_iconInit", AccessTools.all).SetValue(this, true);
+            stack = 0;
         }
     }
 }
